4efce0fb16070ea90dac942e674b2025451b2fbd
006Darsh <21ce006@charusat.edu.in>
2023-07-24 12:54:42 +0530

    router

--- NodeJs Server/NodemonDemo/task2.js
+++ NodeJs Server/NodemonDemo/task2.js
@@ -1,61 +1,14 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const router = require("./task2Router"); // Assuming the router.js file is in the same directory.
+require("dotenv").config();
 
-const hostname = "127.0.0.1";
-const port = 3000;
-// New app using express module
+const hostname = process.env.HOSTNAME;
+const port = process.env.PORT;
 const app = express();
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/html/task2.html");
-});
+app.use("/", router);
+app.use("/display", router);
 
-app.post("/display", function (req, res) {
-  var fname = req.body.fname;
-  var lname = req.body.lname;
-  var id = req.body.id;
-  var email = req.body.email;
-  var phone = req.body.phone;
-  var date = req.body.date;
-  var phone = req.body.phone;
-  var gender = req.body.gender;
-  var state = req.body.state;
-  var range = req.body.range;
-  var time = req.body.time;
-  var hobbie = req.body.hobbie;
-  var ele_subject = req.body.ele_subject;
-  var ele_sub_code = req.body.ele_sub_code;
-  var Elective_subject = ele_sub_code + " " + ele_subject;
-
-  var FullName = fname + " " + lname;
-  var collegeinfo = {
-    "Full Name: ": FullName,
-    "Birth Date: ": date,
-    "Gender: ": gender,
-    "Id: ": id,
-    "Email: ": email,
-    "Phone No: ": phone,
-    "CGPA: ": range,
-    "Elective Subject: ": Elective_subject,
-    "Home State: ": state,
-    "Current Time: ": time,
-    "Hobbies: ": hobbie,
-  };
-  var responseString = "";
-  for (const keys in collegeinfo) {
-    responseString += `<b>${keys}</b>${collegeinfo[keys]}<br\>`;
-  }
-  res.send(responseString);
-  //   res.send(
-  //     `Full Name: ${FullName}<br/>Email: ${email}<br\>Phone No: ${phone}<br/>Elective Subject: ${Elective_subject}`
-  //   );
-});
-
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log(`server is running on http://${hostname}:${port}/`);
 });
