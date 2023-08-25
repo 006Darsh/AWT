d5bfa36f9d798664a493b61019c958e375d7c469
006Darsh <21ce006@charusat.edu.in>
2023-08-25 09:50:55 +0530

    commit

--- ECMASCRIPT_Demo/Async_teams.js
+++ ECMASCRIPT_Demo/Async_teams.js
@@ -1,9 +1,9 @@
 function Sync(){
 
 console.log("Welcome to Sync Program");
-//     setTimeout(()=>{
-//         console.log("Welcome to Sycn Program");
-//     },5000);
+    setTimeout(()=>{
+        console.log("Welcome to Sycn Program");
+    },5000);
 }
 console.log("Start");
 Sync(); //5sec
