a5d1769d7783693ce57c3b501e6617291580e4ea
006Darsh <21ce006@charusat.edu.in>
2023-09-25 12:36:59 +0530

    submit

--- Personal-Info-Project/Personal-Info-api/Routes/PersonalInfoRoutes.js
+++ Personal-Info-Project/Personal-Info-api/Routes/PersonalInfoRoutes.js
@@ -1,7 +1,11 @@
-const { createPersonalInfo } = require("../Controllers/PersonalInfoController");
+const { createPersonalInfo, GetAllInfo, GetInfobyId, UpdatePersonalInfo, DeletePersonalInfo } = require("../Controllers/PersonalInfoController");
 
 const PersonalInfoRoutes = (app) => {
-  app.post("/add/personalinfo", createPersonalInfo);
+    app.post("/add/personalinfo", createPersonalInfo);
+    app.get("/allInfos", GetAllInfo);
+    app.get("/Info/:id", GetInfobyId);
+    app.put("/Info/update-info/:id", UpdatePersonalInfo);
+    app.delete("/Info/delete-Info/:id", DeletePersonalInfo);
 };
 
 module.exports = PersonalInfoRoutes;
